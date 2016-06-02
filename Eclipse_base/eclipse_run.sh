52fd56b3139d07f959d99af371a8105c3bb0d3d3
rafd <ct@tommasitommasi.com>
2016-06-02 21:57:53 +0200

    succesful chain building cleaned code

--- Eclipse_base/eclipse_run.sh
+++ Eclipse_base/eclipse_run.sh
@@ -1,12 +1,3 @@
-#docker run -it \
-#    --env="DISPLAY" \
-#    --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
-#    osrf/ros:indigo-desktop-full \
-#    rqt
-#export containerId='docker ps -l -q'
-
-#xhost +
-#my_homedir=$(~)
 my_homedir=${HOME}
 
 echo "my_homedir:$my_homedir"
@@ -16,13 +7,7 @@ docker run -ti --rm \
 	-v /tmp/.X11-unix:/tmp/.X11-unix \
 	-v $my_homedir:/workspace \
 	-v $my_homedir/.eclipse-docker:/home/developer \
-	-v /home/rafd/localSVN:/home/developer/localSVN \
-	-v /usr/local/share/perl/5.20.2/:/usr/local/share/perl/5.20.2/ \
-	rafdouglas/mars_base
+	rafdouglas/eclipse_docker:Eclipse_base
 
 #	-v $HOME/.Xauthority:/home/developer/.Xauthority \
 #	--net=host \
-
-#	-v `pwd`:/workspace \
-#	-v `pwd`/.eclipse-docker:/home/developer \
-
